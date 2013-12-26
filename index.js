e2cc915840476d711e109e62f79ac209d620b0bf
XueFei <compleat@gmail.com>
2013-12-26 22:52:15 +0800

    Crawler example: www.nocancer.com.cn crawler implements.

--- index.js
+++ index.js
@@ -12,13 +12,20 @@ console.log('Set process name :', config.appName);
 
 process.title = config.appName;
 
-var ctp = require('../node-cookies.txt/index.js');
+var ctp = require('cookies.txt/index.js');
 var crawler = require("./SmartCrawler.js");
-var discuzX2Plugin = require('./plugins/discuzX2.js');
-//finished 23132
+var discuzX2Plugin = require('./plugins/discuz/discuzX2.js');
+
+//Load cookies
 ctp.parse(__dirname + '/config/cookies.txt', function (cookies) {
+
+  //Init crawler with options.
   crawler.init({jar:ctp, callback:discuzX2Plugin, update:true, updateFlag:1231});
+
+  //Crawl the url
   crawler.crawl('http://www.nocancer.com.cn/forum.php');
+
+  //Add other urls
   crawler.push('http://www.nocancer.com.cn/portal.php');
   crawler.push('http://www.nocancer.com.cn/thread-2070-1-1.html');
   //crawler.crawl('http://www.nocancer.com.cn/thread-9837-1-1.html');
